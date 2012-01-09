0bb9440016441fae9d51862d1ae40ab1496967f7
Craig Smitham <craigsmitham@gmail.com>
2012-01-09 10:12:02 -0600

    updated assignment code

---
+++ ruby/assignment.rb
@@ -0,0 +1,9 @@
+def assignment(title, &block)
+  puts "\n\n--"
+  puts title
+  puts
+  block.call
+end
+# QUESTION: How would I call the assignment method using brackets {} for the code block?
+
+alias :bonus :assignment
--- ruby/day1.rb
+++ ruby/day1.rb
@@ -1,42 +1,49 @@
-puts "\n"+'Print the string "Hello, world":'
-puts "Hello World"
+load 'assignment.rb' 
 
-puts "\n"+'For the string "Hello, Ruby," find the index of the word "Ruby":'
-"Hello, Ruby".index("Ruby")
-
-puts "\nPrint your name ten times:"
-puts 'What is your name?'
-name = gets
-10.times do
-  puts name
+assignment 'Print the string "Hello, world"' do
+  puts "Hello World"
 end
 
-puts "\n"+'Pring the string "This sentence number 1," where the number 1 changes from 1 to 10'
-x = 1
-until x > 10
-  puts "This sentence number #{x}"
-  x = x + 1
+assignment 'For the string "Hello, Ruby," find the index of the word "Ruby"' do
+  "Hello, Ruby".index("Ruby")
 end
 
-puts "\nRun a Ruby program from a file:"
-puts "done"
+assignment 'Print your name ten times' do
+  puts 'What is your name?'
+  name = gets
+  10.times { puts name }
+end
 
-puts "\nBONUS: Write a program that picks a number and allow the player to guess"
-rand = rand(100)
-guess = nil
-num_guesses = 0
-until guess == rand
-  print "Enter your guess: "
-  guess = gets.to_i
-  num_guesses += 1
-  if guess == rand
-    puts "***************"
-    puts "CONGRATULATIONS"
-    puts "***************"
-    puts "\nYou guessed in #{num_guesses} guesses"
-  elsif guess < rand
-    puts "\n guess HIGHER"
-  elsif guess > rand
-    puts "\n guess LOWER"
+assignment 'Pring the string "This sentence number 1," where the number 1 changes from 1 to 10' do
+  x = 1
+  until x > 10
+    puts "This sentence number #{x}"
+    x = x + 1
   end
 end
+
+assignment 'Run a Ruby program from a file' do 
+  puts "winning"
+end
+
+bonus "Write a program that picks a number and allow the player to guess" do
+  rand = rand(100)
+  guess = nil
+  num_guesses = 0
+  until guess == rand
+    print "Enter your guess: "
+    guess = gets.to_i
+    num_guesses += 1
+    if guess == rand
+      puts "***************"
+      puts "CONGRATULATIONS"
+      puts "***************"
+      puts "\nYou guessed in #{num_guesses} guesses"
+    elsif guess < rand
+      puts "\n guess HIGHER"
+    elsif guess > rand
+      puts "\n guess LOWER"
+    end
+  end
+end
+
